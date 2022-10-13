79f67ea3048da22aa7aab992da7674044d7f910a
YasinU1 <sayedmuhammadyasin.usman@vodafone.com>
2022-10-13 16:30:12 +0100

    Added Essential files

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+class Calculator {
+    constructor(){
+        this.num1 = 0
+        this.num2 = ""
+    }
+    calc(){
+        document.querySelector("#plus").addEventListener("click", this.additon)
+        document.querySelector("#minus").addEventListener("click", this.substraction)
+        document.querySelector("#times").addEventListener("click", this.multiplication)
+        document.querySelector("#divide").addEventListener("click", this.division)
+    }
+    numberValue(){
+        
+    }
+
+    additon(){
+
+    }
+    substraction(){
+
+    }
+    multiplication(){
+
+    }
+    division(){
+
+    }
+    decimalisation(){
+
+    }
+    result(){
+
+    }
+}
